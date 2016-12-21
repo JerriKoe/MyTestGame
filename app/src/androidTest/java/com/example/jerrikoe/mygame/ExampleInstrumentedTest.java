3bf929e1a5bfd15f2701c3cdf6cf34393779b326
Jousef W <Jousef W>
2016-12-21 16:59:56 +0100

    Initial commit

---
+++ app/src/androidTest/java/com/example/jerrikoe/mygame/ExampleInstrumentedTest.java
@@ -0,0 +1,41 @@
+package com.example.jerrikoe.mygame;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.core.deps.guava.base.Strings;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.jerrikoe.mygame", appContext.getPackageName());
+    }
+}
+
+    private String myProperty;
+
+    public String getMyProperty() {
+        return myProperty;
+    }
+
+    public void setMyProperty(String myProperty) {
+        this.myProperty = myProperty;
+    }
+
+    private void DoSomething(String someValue){
+        setMyProperty(someValue);
+    }
